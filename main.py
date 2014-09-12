c758058d2c557d87203e46833f0a1f2fb2b9adb2
Ax3 <axeeffect1234@gmail.com>
2014-09-12 22:05:07 +0100

    Added weather and map locations, cleared messages code

--- main.py
+++ main.py
@@ -9,15 +9,23 @@ import random
 from datetime import datetime
 from random import randint
 from configobj import ConfigObj
+
+weather_disable = 0 
+
+print "Initializing..."
 config = ConfigObj("settings.ini")
 vk_access_token = config['vk_token']
+geocodingAPI = config['google_geocoding']
+forecastioAPI = config['forecastio']
+
+
 attempt_id = 0
 chat_id = 0
-
 #### Settings
-customMsg = "\n //ax3bot"
+customMsg = "\n //ax3bot "
 show_names = 0 # 1 or 0, disable it for better performance
 #### Settings
+connect_success = 0
 
 
 vkapi = vk.API(access_token=vk_access_token)
@@ -32,25 +40,60 @@ def msgcheck(msg):
     global attempt_id
     attempt_id = attempt_id + 1 # VK anti-block system
     if msg.find("test") == 0:
-        vk_message = "Рандом: " + str(randint(2,100)) + "%." + "\n id: " + str(attempt_id)
+        vk_message = "Рандом: {}".format(str(randint(2,100))) + "%."
         msgsend(userid, vk_message, chat_id)
     elif msg.find("привет") == 0:
-        vk_message = "Привет!" + "\n id: " + str(attempt_id)
+        vk_message = "Привет!" 
         msgsend(userid, vk_message, chat_id)
     elif msg.find("курс") == 0:
         kurs = requests.get("http://api.fixer.io/latest?base=USD")
         print kurs.json()
         kursbid1 = kurs.json()["rates"]
         kursbid = kursbid1["RUB"]
-        vk_message = "Курс рубля к доллару: " + str(kursbid) + " рублей = 1$" + "\n id: " + str(attempt_id)
+        vk_message = "Курс рубля к доллару: {} рублей = 1$.".format(kursbid)
         msgsend(userid, vk_message, chat_id)
-    #elif msg.find("погода") == 0:
-        #obs = owm.weather_at('Moscow,ru')
-        #w = obs.get_weather()
-        #temp = w.get_temperature(unit='celsius')
-        #vk_message = "Температура в Москве: " + str(temp["temp"]) + " градусов." + "\n id: " + str(attempt_id)
-        #msgsend(userid, vk_message, chat_id)
-        #pass
+    elif msg.find("погода") == 0:
+        if weather_disable == 0:
+            print "got it!"
+            msgGeoSplit = msg.split()
+            msgGeoSplit = msgGeoSplit[1:]
+            msgGeoString = ' '.join(msgGeoSplit)
+            print msgGeoString
+            geocodeURL = "https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}".format(msgGeoString, geocodingAPI)
+            print geocodeURL
+            geocodeRequest = requests.get(geocodeURL)
+            geoResult2 = geocodeRequest.json()
+
+            if geoResult2["status"] == "OK":
+                geoResult = geoResult2["results"]
+                geoLocation1 = geoResult[0]["geometry"]
+                geoLocation2 = geoLocation1["location"]
+                geoLat = geoLocation2["lat"]
+                geoLng = geoLocation2["lng"]
+                print "Lat: {}, Lng: {}".format(geoLat, geoLng)
+
+                forecastURL = "https://api.forecast.io/forecast/{}/{},{}?units=si".format(forecastioAPI, geoLat, geoLng)
+                #print forecastURL
+                forecastGet = requests.get(forecastURL)
+                forecastJSON = forecastGet.json()
+                #print forecastJSON
+                forecastp1 = forecastJSON["currently"]
+                forecastp2 = forecastp1["temperature"]
+                forecastp2Summary = forecastp1["summary"]
+                forecastp2Windspeed = forecastp1["windSpeed"]
+                forecastp2Hum = forecastp1["humidity"]
+                forecastp2Pressure = forecastp1["pressure"]
+                vk_message = "Погода: {}\nLat: {}, Lng: {} \nhttp://maps.google.co.uk/maps/@{},{},16z \n Температура воздуха: {}°C \n {} \n Скорость ветра: {}м/c \n Влажность: {}% \n Давление: {}".format(msgGeoString, geoLat, geoLng, geoLat, geoLng, forecastp2, forecastp2Summary, forecastp2Windspeed, forecastp2Hum*100, forecastp2Pressure)
+                msgsend(userid, vk_message, chat_id)
+
+            else:
+                vk_message = "Не знаю такого места!"
+                msgsend(userid, vk_message, chat_id)
+
+
+
+            #vk_message = "successful!"
+            #msgsend(userid, vk_message, chat_id)
     elif msg.find("сосчитать") == 0:
         msgCalc = msg.split()
         msgCalc = msgCalc[1:]
@@ -58,19 +101,19 @@ def msgcheck(msg):
         try:
             if msgCalc[1] == "+":
                 msgCalcResult = int(msgCalc[0]) + int(msgCalc[2])
-                vk_message = "Результат: " + str(msgCalcResult)
+                vk_message = "Результат: {}".format(msgCalcResult)
                 msgsend(userid, vk_message, chat_id)
             elif msgCalc[1] == "-":
                 msgCalcResult = int(msgCalc[0]) - int(msgCalc[2])
-                vk_message = "Результат: " + str(msgCalcResult)
+                vk_message = "Результат: {}".format(msgCalcResult)
                 msgsend(userid, vk_message, chat_id)
             elif msgCalc[1] == "*":
                 msgCalcResult = int(msgCalc[0]) * int(msgCalc[2])
-                vk_message = "Результат: " + str(msgCalcResult)
+                vk_message = "Результат: {}".format(msgCalcResult)
                 msgsend(userid, vk_message, chat_id)
             elif msgCalc[1] == "/":
                 msgCalcResult = int(msgCalc[0]) / int(msgCalc[2])
-                vk_message = "Результат: " + str(msgCalcResult)
+                vk_message = "Результат: {}".format(msgCalcResult)
                 msgsend(userid, vk_message, chat_id)
             else:
                 vk_message = "Неправильный ввод! Вы ввели: " + str(msg)
@@ -96,14 +139,14 @@ def msgsend(userid, message, chatid):
     try:
         atest = result2[7]["from"]
         try:
-            message = message + customMsg
+            message = message + customMsg + str(attempt_id)
             vkapi.messages.send(chat_id = chat_id, message = message)
         except Exception:
             print "Message send error!"
             pass
     except KeyError:
         try:            
-            message = message + customMsg
+            message = message + customMsg + str(attempt_id)
             vkapi.messages.send(message = message, user_id = userid)
         except Exception:
             traceback.print_exc()
@@ -113,7 +156,7 @@ def msgsend(userid, message, chatid):
 
 
 urlstring = "http://" + str(asd["server"]) + "?act=a_check&key=" + str(asd["key"]) + "&ts=" + str(asd["ts"]) + "&wait=25&mode=2"
-print "Ax3 VK Bot activated!"
+print "Connecting..."
 
 while True:
     try:
@@ -122,7 +165,11 @@ while True:
         response = requests.get(urlstring)
         result = ast.literal_eval(response.content)
         string2 = response.content[6]
+        if connect_success == 0:
+            print "Ax3 Bot successfully connected!"
+            connect_success = 1
     except Exception:
+        traceback.print_exc()
         pass
     try:
         result2 = result["updates"][0]
